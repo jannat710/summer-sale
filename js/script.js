6e741ce4de70766ea77375e202b6fe0eb4a89a82
e <jannatul0040@gmail.com>
2023-08-21 09:01:39 +0600

    added discount price

--- js/script.js
+++ js/script.js
@@ -6,7 +6,20 @@ function handleCLikBtn(target) {
     li.innerText = itemName;
     selectedItemContainer.appendChild(li);
     const totalPrice = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    
+    // Total Amount
     total = parseFloat(total) + parseFloat(totalPrice);
     const inDecimalTotalPrice = total.toFixed(2);
     document.getElementById("total-price").innerText = inDecimalTotalPrice;
+
+    // Discount Amount
+    const discount = inDecimalTotalPrice * 0.2;
+    const inDecimalDiscount =  discount.toFixed(2);
+    document.getElementById("discount-price").innerText = inDecimalDiscount;
+
+    // New Amount
+    const newTotalPrice = inDecimalTotalPrice - inDecimalDiscount;
+    const inDecimalNewTotal = newTotalPrice.toFixed(2);
+    document.getElementById("new-price").innerText = inDecimalNewTotal;
+
 }
